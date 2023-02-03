76867f01328d8c1bdec6c1850418bc7e69d12f85
kfiddle <kenjfiddle@gmail.com>
2023-02-03 09:43:30 -0500

    various

--- src/components/gigs/gigs/GigDeets.js
+++ src/components/gigs/gigs/GigDeets.js
@@ -1,23 +1,24 @@
 import { Fragment, useState } from "react";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/esm/ListGroup";
-import ListGroupItem from "react-bootstrap/esm/ListGroupItem";
-import Offcanvas from "react-bootstrap/Offcanvas";
 
 import styles from "./GigDeets.module.css";
+import PossiblePlayers from "./PossiblePlayers";
 
 const GigDeets = ({ gig }) => {
   const { venue, address, date, parts } = gig;
-  const [instClicked, setInstClicked] = useState({ instName: null, idx: null });
+  const [openCanvas, setOpenCanvas] = useState(false);
+  const [partClicked, setPartClicked] = useState(null);
+
+  const partClicker = (part, idx) => () => {
+    setPartClicked(part);
+    setOpenCanvas(true);
+  };
 
   const displayParts = parts.map((part, idx) => (
-    <ListGroup.Item
-      key={idx}
-      action
-      onClick={() => setInstClicked({ instName: part.instName, idx })}
-    >
+    <ListGroup.Item key={idx} action onClick={partClicker(part, idx)}>
       <p
-        // style={instClicked.idx === idx ? { color: "blue" } : { color: "black" }}
+      // style={instClicked.idx === idx ? { color: "blue" } : { color: "black" }}
       >
         {part.instName}
       </p>
@@ -34,15 +35,9 @@ const GigDeets = ({ gig }) => {
         </Card.Body>
       </Card>
 
-      <Offcanvas
-        show={instClicked !== null ? true : false}
-        onHide={() => setInstClicked(null)}
-        placement="end"
-      >
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>{instClicked.instName}</Offcanvas.Title>
-        </Offcanvas.Header>
-      </Offcanvas>
+      {openCanvas && (
+        <PossiblePlayers part={partClicked} setOpenCanvas={setOpenCanvas} />
+      )}
     </Fragment>
   );
 };
---
+++ src/components/gigs/gigs/PossiblePlayers.js
@@ -0,0 +1,26 @@
+import Offcanvas from "react-bootstrap/Offcanvas";
+import ListGroup from "react-bootstrap/esm/ListGroup";
+
+import styles from "./PossiblePlayers.module.css";
+import useGrabList from "../../../hooks/useGrabList";
+
+const PossiblePlayers = ({ part, setOpenCanvas }) => {
+  // const possiblePlayers = useGrabList('players')
+
+  return (
+    <Offcanvas show={true} onHide={() => setOpenCanvas(false)} placement="end">
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title>
+          <h2>{part.instName}</h2>
+        </Offcanvas.Title>
+      </Offcanvas.Header>
+      <ListGroup>
+        <ListGroup.Item action>a</ListGroup.Item>
+        <ListGroup.Item action>b</ListGroup.Item>
+        <ListGroup.Item action>c</ListGroup.Item>
+      </ListGroup>
+    </Offcanvas>
+  );
+};
+
+export default PossiblePlayers;
